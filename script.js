0af4da2369a930da31bf9acb74dc364748a6d5d0
Michael A Nowiszewski <micski@gmail.com>
2017-12-24 21:42:32 +0000

    update 2 to js xmas eve

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
 			var testNumLength = function(number) {
 				if (number.length > 9) {
 					totaldiv.text(number.substr(number.length - 9, 9));
+					if (number.length > 15) {
+						number = "";
+						totaldiv.text("Err");
+					}
+				}
+			};
 					
 //help me JS, you're my only hope!!
 
