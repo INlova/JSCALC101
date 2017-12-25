0985a8a8dfc7386277d6f41a36e18238d537dcd6
Michael A Nowiszewski <micski@gmail.com>
2017-12-25 00:23:38 +0000

    update js formatting

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
-	var testNumLength = function(number) {
-        if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
-            if (number.length > 15) {
-                number = "";
-                totaldiv.text("Err");
-            }
-        } 
-    };
-	var number = "";
-    var newnumber = "";
-    var operator = "";
-    var totaldiv = $("#total");
-    totaldiv.text("0");
+$(document).ready(function() {
+			var testNumLength = function(number) {
+				if (number.length > 9) {
+					totaldiv.text(number.substr(number.length - 9, 9));
+					if (number.length > 15) {
+						number = "";
+						totaldiv.text("Err");
+					}
+				}
+			};
+			var number = "";
+			var newnumber = "";
+			var operator = "";
+			var totaldiv = $("#total");
+			totaldiv.text("0");
 
-//help me JS, you're my only hope!!
+//help me JS, you're my only hope!! Just saw the LAST JEDI!!
 
 
 
