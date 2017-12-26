54041946810d1fd7b813ab13af18810d767471ec
Michael A Nowiszewski <micski@gmail.com>
2017-12-26 22:19:53 +0000

    update js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {// function ready onload
+$(document).ready(function() { // function ready onload
 			var testNumLength = function(number) { //numlength var, number function 
 				if (number.length > 9) { //for loop, inputs >9 inclusiveof 0
 					totaldiv.text(number.substr(number.length - 9, 9));
@@ -11,56 +11,52 @@ $(document).ready(function() {// function ready onload
 			var number = ""; //current number must be variable not fixed
 			var newnumber = ""; // num being entered must be variable too
 			var operator = ""; //must also be variable to include all 4! +, -, /, and *.
-			var totaldiv = $("#total");// runnning count in totaldiv
+			var totaldiv = $("#total"); // runnning count in totaldiv
 			totaldiv.text("0");
-			
-$("#numbers a").not("#clear,#clearall").click(function() {
-	number += $(this).text();
-	totaldiv.text(number);
-	testNumLength(number);
-});
+			$("#numbers a").not("#clear,#clearall").click(function() {
+				number += $(this).text();
+				totaldiv.text(number);
+				testNumLength(number);
+			});
+			$("#clear,#clearall").click(function() { //function to clear and clear all - set to null ""
+				number = "";
+				totaldiv.text("0");
+				if ($(this).attr("id") === "clearall") {
+					newnumber = "";
+				}
+			});
+			$("#operators a").not("#equals").click(function() {
+				operator = $(this).text();
+				newnumber = number;
+				number = "";
+				totaldiv.text("0");
+			});
+			//Add your last .click() here!
+			$("#equals").click(function() { //onclick function runs the calc inputs
+						/*help me JS, you're my only hope!! Just saw the LAST JEDI!!
 
- $("#clear,#clearall").click(function() {
- 	number = "";
- 	totaldiv.text("0");
- 	if ($(this).attr("id") === "clearall") {
- 		newnumber = "";
- 	}
- });
- 
-$("#operators a").not("#equals").click(function() {
-	operator = $(this).text();
-	newnumber = number;
-	number = "";
-	totaldiv.text("0");
-});
-
-//Add your last .click() here!
-    $("#equals").click(function(){ //onclick function runs the calc inputs
-/*help me JS, you're my only hope!! Just saw the LAST JEDI!!
-
-parseInt to string for each operator? No scuts here?
+						parseInt to string for each operator? No scuts here?
 
 
 
-/*               |||      |||    
-               | |  __  | |
-|-|_____-----/   |_|  |_|   \-----_____|-|
-|_|_________{   }|  (^) |{  }__________|_|  
- ||          |_| |   ^  | |_|          ||  
- |              \|  /\  |/              |  
- |               \ |--| /               |    
- =               \ |__| /               =    
- +               \      /               +
-                  \    /    
-                  \    /    
-                   \  /
-                   \  /
-                   \  /
-                   \  /
-                   \  /    
-                   \  /
-                    \/
-                    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
-                    https://www.codecademy.com/en/forum_questions/505814cfcb744300020146e3
-                    .
+						/*               |||      |||    
+						               | |  __  | |
+						|-|_____-----/   |_|  |_|   \-----_____|-|
+						|_|_________{   }|  (^) |{  }__________|_|  
+						 ||          |_| |   ^  | |_|          ||  
+						 |              \|  /\  |/              |  
+						 |               \ |--| /               |    
+						 =               \ |__| /               =    
+						 +               \      /               +
+						                  \    /    
+						                  \    /    
+						                   \  /
+						                   \  /
+						                   \  /
+						                   \  /
+						                   \  /    
+						                   \  /
+						                    \/
+						                    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+						                    https://www.codecademy.com/en/forum_questions/505814cfcb744300020146e3
+						                    .
